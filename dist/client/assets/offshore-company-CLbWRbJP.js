46c9ab06744fd5e7cc18e10241be3d59b7dabfe1
Your Name <your-github-email@example.com>
2026-05-14 19:35:30 +0800

    fix: remove dist from git tracking

--- dist/client/assets/offshore-company-CLbWRbJP.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-Dxcrbb5r.js";import{S as t}from"./ServicePlaceholder-DL0kqpmB.js";import"./arrow-right-CrQM0SN1.js";const m=()=>o.jsx(t,{titleKey:"sub_offshore"});export{m as component};
